f8569f64dfd7ac35bb9bd56aa59c88fab7674c99
jerry <jdinardo@nycap.rr.com>
2014-01-09 16:01:17 -0500

    parsing input with My.Parse

--- net/http.hs
+++ net/http.hs
@@ -13,9 +13,6 @@ import My.Net
 import My.Time
 import My.Parse
 
-portnum = 8080
-root = "/www"
-
 notFound fn=("404 Not Found",
           "The requested URL " ++ fn ++ " was not found on this server.")
 forbidden fn=("403 Forbidden",
@@ -49,42 +46,35 @@ getMimeType fn = if null xs then [] else (snd.head) xs  where
     xs = filter (\x->isSuffixOf (fst x) fn) extensions
 
 getStatic h fn = do
-    let fn' = if fn == "/" then root ++ "/index.html" else (root ++ fn)
-        m = getMimeType fn
-        bad = isInfixOf ".." fn
-    e <- doesFileExist fn'
-
-    if m==[]  || bad  then errmsg h $ forbidden fn
-    else if not e then errmsg h $ notFound fn'
+    let m = getMimeType fn
+    if m==[] then errmsg h $ forbidden fn
     else do
-        f <- B.readFile fn'
+        f <- B.readFile fn
         B.hPut h $ B.pack $
          "HTTP/1.1 200 OK\nConnection: close\nContent-Type: " ++ m ++ "\n\n"
         B.hPut h f
 
-getCgi h fn llbuf = do
-    let bad = isInfixOf ".." fn
-    e <- doesFileExist $ root ++ fn
-    if bad  then errmsg h $ forbidden fn
-    else if not e then errmsg h $ notFound fn
-    else do
-        (fd0,fd1,fd2,pid) <- runInteractiveProcess (root++fn) [] Nothing Nothing
-        --mapM hClose [fd0,fd2]
-        d <- B.hGetContents fd1
-        B.hPut h (B.append "HTTP/1.1 200 OK\nConnection: close\n" d)
-        waitForProcess pid
-        --B.putStr (B.append "HTTP/1.1 200 OK\nConnection: close\n" d)
-        return ()
+getCgi h fn = do
+    (fd0,fd1,fd2,pid) <- runInteractiveProcess fn [] Nothing Nothing
+    --mapM hClose [fd0,fd2]
+    d <- B.hGetContents fd1
+    B.hPut h (B.append "HTTP/1.1 200 OK\nConnection: close\n" d)
+    waitForProcess pid
+    --B.putStr (B.append "HTTP/1.1 200 OK\nConnection: close\n" d)
     return ()
-
+--
+-- Parse Input
+--
 parseInput ubuf = do
-    let prc = runParse parseInput2 ubuf
-    print prc
+    let rc = runParse parseInput2 ubuf
+    return rc
 
+--parseInput2 :: Parse String (String,String,String,[[Char],String],String)
 parseInput2 = do
-    (w1,w2,w3) <- getHdr
+    (h1,h2,h3) <- getHdr
     hs <- many1 httpData
-    return (w1,w2,w3,hs)
+    ds <- onLeft "" (eol >> toEod)
+    return (h1,h2,h3,hs,ds)
 
 httpData = do
     d1 <- many1 (noneOf ":")
@@ -101,43 +91,35 @@ getHdr  = do
     eol
     return (w1,w2,w3)
 
-
-serverproc :: Handle -> IO ()
-serverproc h = do
+--
+-- SERVERPROC
+--
+serverproc :: String -> Handle -> IO ()
+serverproc root h = do
     (rc, buf) <- netGet h 4200 2000
     let ubuf = B.unpack buf
-    prc <- parseInput ubuf
+    pdata <- parseInput ubuf
+    case pdata of
+        Left err -> print err
+        Right ((iop,ifn,iver,ihs,i_ds), i_st) -> do
+            let (fn',qstr) = span (/= '$') ifn
+                bad = isInfixOf ".." fn'
+                fn = if fn' == "/" then root ++ "/index.html" else (root ++ fn')
+            e <- doesFileExist $ fn
+            if bad  then errmsg h $ forbidden fn
+            else if not e then print "NOTFOUND" >> (errmsg h $ notFound fn)
+            else do
+                perms <- getPermissions (fn)
+                if isPrefixOf "GET" (map toUpper iop) then do
+                    if (executable perms) then getCgi h fn
+                    else getStatic h fn
+                else if isPrefixOf "POST" (map toUpper iop) then do
+                    if (executable perms) then getCgi h fn
+                    else getStatic h fn
+                else errmsg h $ forbidden ("command " ++ iop ++ " not implemented")
     return ()
 
-{-
-        lbuf = lines ubuf
-
-    putStrLn ""
-    mapM putStrLn lbuf
-    print $ take 3 $ words ubuf
-
-    case (words ubuf) of
-        (op:ifn:_) -> do
-            let (fn,qstr) = span (/= '$') ifn
-                bad = isInfixOf ".." fn
-            --print (op,ifn,fn,qstr)
-            e <- doesFileExist $ root ++ fn
-            perms <- getPermissions (root++fn)
-
-            if bad  then errmsg h $ forbidden fn
-            else if not e then errmsg h $ notFound fn
-            else if isPrefixOf "GET" (map toUpper op) then do
-                if (executable perms) then getCgi h fn lbuf
-                else getStatic h fn
-            else if isPrefixOf "POST" (map toUpper op) then do
-                if (executable perms) then getCgi h fn lbuf
-                else getStatic h fn
-            else return ()
-        _ -> return ()
-
-getContent buf = if B.length s11 < B.length s21  then s12  else s22  where
-    (s11,s12) = B.breakSubstring "\r\n\r\n" buf
-    (s21,s22) = B.breakSubstring "\n\n" buf
--}
-
-main = server portnum serverproc
+main = do
+    let portnum = 8080
+        root = "/www"
+    server portnum (serverproc root)
